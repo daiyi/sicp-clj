4a119ee9e76808018d701aeeb6123d97a36467ca
chris⚡  (daiyi) <daiyitastic@gmail.com>
2016-11-03 20:21:50 +0100

    sqrt

--- src/chapters/chapter_1.clj
+++ src/chapters/chapter_1.clj
@@ -1,12 +1,51 @@
 (ns chapters.chapter-1)
 
+(defn sq
+  [x]
+  (* x x))
+
 (defn abs
   [x]
   (cond (< x 0) (- x)
         (>= x 0) x))
 
+(defn a-plus-abs-b
+  [a b]
+  ((if (> b 0) + -) a b))
+
+(defn sum-of-squares
+  [x y]
+  (+ (sq x)
+     (sq y)))
+
 (defn sum-of-max-squares
   "takes three numbers as arguments and returns the sum of the squares of the two larger numbers."
   [x, y, z]
-  (- (+ x y z) (min x y z)))
+  (cond (and (<= x y) (<= x z)) (sum-of-squares y z)
+        (and (<= y z) (<= y x)) (sum-of-squares x z)
+        (and (<= z x) (<= z y)) (sum-of-squares x y)))
 
+(defn average
+  [x y]
+  (/ (+ x y)
+     2))
+
+(defn improve-guess
+  [guess x]
+  (average guess (/ x guess)))
+
+(defn good-enough?
+  [guess x]
+  (> 0.00001
+     (abs (- (sq guess)
+             x))))
+
+(defn sqrt-guess
+  [guess x]
+  (if (good-enough? guess x)
+    guess
+    (sqrt-guess (improve-guess guess x)
+                x)))
+(defn sqrt
+  [x]
+  (sqrt-guess 1.0 x))
--- test/sicp_clj/chapter_1_test.clj
+++ test/sicp_clj/chapter_1_test.clj
@@ -8,8 +8,15 @@
       (is (= (abs -4) 4))
       (is (= (abs 0) 0))
       (is (= (abs 3) 3)))
-    (testing "1.3"
-      (is (= 5 (sum-of-max-squares 3 2 1)))
-      (is (= 4 (sum-of-max-squares 0 0 4)))
-      (is (= -2 (sum-of-max-squares -5 0 -2))))
-    ))
+    (testing "1.3 - sum of max squares"
+      (is (= 13 (sum-of-max-squares 3 2 1)))
+      (is (= 16 (sum-of-max-squares 0 0 4)))
+      (is (= 4 (sum-of-max-squares -5 0 -2)))
+      (is (= 8 (sum-of-max-squares 2 2 2))))
+    (testing "1.4 - operators as compound expressions"
+      (is (= (- 1)
+             (a-plus-abs-b (- 2) (- 1)))))
+    (testing "sqrt"
+      (is (< (abs (- (sq (sqrt 2))
+                     2))
+             0.00001)))))
